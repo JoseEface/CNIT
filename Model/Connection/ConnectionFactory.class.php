5c8f3826452228c4c6e9e74e77b1300ed9915695
José.Eface <estudodrive@hotmail.com>
2018-09-16 17:01:58 -0300

    Comentário adicionado em relação a conta do BD

--- Model/Connection/ConnectionFactory.class.php
+++ Model/Connection/ConnectionFactory.class.php
@@ -5,6 +5,7 @@ namespace Model\Connection;
 class ConnectionFactory {
 
     public static function getConnection() {
+	//Conta genérica, deve ser substituída na conta oficial
         return new \PDO("mysql:dbname=cnit;host=localhost","admin","hlnomysql1");
     }
 
